861728259d46686cae83865ce15dd1b7c3b92146
Irawan <107756910+irawandwiarno@users.noreply.github.com>
2026-06-13 20:07:26 +0700

    feat: routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import './App.css'
 import HomePage from './pages/home/home'
-
+import SutjiPage from './pages/sutji/sutji'
 
 function App() {
   return (
-    <>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Toaster />
-      <HomePage />
-    </>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/sutji" element={<SutjiPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
